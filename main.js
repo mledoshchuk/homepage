34282ad382fe3622bd762dae9d1d09f643602f7f
unknown <mledoshchuk@gmail.com>
2023-06-18 16:42:12 +0300

    Validated index.html, main.js, styles.css files

--- main.js
+++ main.js
@@ -1,85 +1,75 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    const timelineSections = document.querySelectorAll('.timeline');
-  
-    timelineSections.forEach(section => {
-      const icon = section.querySelector('.icon');
-      const contentDiv = section.querySelector('.content');
-      const originalContent = contentDiv.innerHTML;
-  
-      icon.addEventListener('click', function handleClick(event) {
-        let className = icon.className;
-        let newContent = '';
-  
-        if (className.includes("fa-cake-candles")) {
-          newContent = '<img src="assets/cat-cute.gif" alt="Hello">';
-        }
-        if (className.includes("fa-school")) {
-            newContent = '<img src="assets/school.gif" alt="School">';
-        }
-        if (className.includes("fa-graduation-cap")) {
-        newContent = '<img src="assets/university.gif" alt="University">';
-        }
+document.addEventListener('DOMContentLoaded', function() {
+	const timelineSections = document.querySelectorAll('.timeline');
 
-        if (className.includes("fa-cat")) {
-        newContent = '<img src="assets/cat.webp" alt="Cat">';
-        }
+	timelineSections.forEach(section => {
+		const icon = section.querySelector('.icon');
+		const contentDiv = section.querySelector('.content');
+		const originalContent = contentDiv.innerHTML;
 
-        if (className.includes("fa-briefcase")) {
-        newContent = '<img src="assets/first-job.gif" alt="First Job">';
-        }
+		icon.addEventListener('click', function handleClick(event) {
+			let className = icon.className;
+			let newContent = '';
 
-        if (className.includes("fa-laptop")) {
-        newContent = '<img src="assets/monkey.gif" alt="Monkey">';
-        }
-    
-  
-        if (contentDiv.classList.contains('image-visible')) {
-          contentDiv.innerHTML = originalContent;
-          contentDiv.classList.remove('image-visible');
-        } else {
+			if (className.includes("fa-cake-candles")) {
+				newContent = '<img src="assets/cat-cute.gif" alt="Hello">';
+			}
+			if (className.includes("fa-school")) {
+				newContent = '<img src="assets/school.gif" alt="School">';
+			}
+			if (className.includes("fa-graduation-cap")) {
+				newContent = '<img src="assets/university.gif" alt="University">';
+			}
 
-          contentDiv.innerHTML = newContent;
-          contentDiv.classList.add('image-visible');
-        }
-      });
+			if (className.includes("fa-cat")) {
+				newContent = '<img src="assets/cat.webp" alt="Cat">';
+			}
 
-      icon.addEventListener('mouseover', function handleClick(event) {
-        let className = icon.className;
-        let newContent = '';
-  
-        if (className.includes("fa-cake-candles")) {
-          newContent = '<img src="assets/cat-cute.gif" alt="Hello">';
-        }
-        if (className.includes("fa-school")) {
-            newContent = '<img src="assets/school.gif" alt="School">';
-        }
-        if (className.includes("fa-graduation-cap")) {
-        newContent = '<img src="assets/university.gif" alt="University">';
-        }
+			if (className.includes("fa-briefcase")) {
+				newContent = '<img src="assets/first-job.gif" alt="First Job">';
+			}
 
-        if (className.includes("fa-cat")) {
-        newContent = '<img src="assets/cat.webp" alt="Cat">';
-        }
+			if (className.includes("fa-laptop")) {
+				newContent = '<img src="assets/monkey.gif" alt="Monkey">';
+			}
 
-        if (className.includes("fa-briefcase")) {
-        newContent = '<img src="assets/first-job.gif" alt="First Job">';
-        }
 
-        if (className.includes("fa-laptop")) {
-        newContent = '<img src="assets/monkey.gif" alt="Monkey">';
-        }
+			if (contentDiv.classList.contains('image-visible')) {
+				contentDiv.innerHTML = originalContent;
+				contentDiv.classList.remove('image-visible');
+			} else {
 
-      });
+				contentDiv.innerHTML = newContent;
+				contentDiv.classList.add('image-visible');
+			}
+		});
 
-    });
-  });
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+		icon.addEventListener('mouseover', function handleClick(event) {
+			let className = icon.className;
+			let newContent = '';
+
+			if (className.includes("fa-cake-candles")) {
+				newContent = '<img src="assets/cat-cute.gif" alt="Hello">';
+			}
+			if (className.includes("fa-school")) {
+				newContent = '<img src="assets/school.gif" alt="School">';
+			}
+			if (className.includes("fa-graduation-cap")) {
+				newContent = '<img src="assets/university.gif" alt="University">';
+			}
+
+			if (className.includes("fa-cat")) {
+				newContent = '<img src="assets/cat.webp" alt="Cat">';
+			}
+
+			if (className.includes("fa-briefcase")) {
+				newContent = '<img src="assets/first-job.gif" alt="First Job">';
+			}
+
+			if (className.includes("fa-laptop")) {
+				newContent = '<img src="assets/monkey.gif" alt="Monkey">';
+			}
+
+		});
+
+	});
+});
