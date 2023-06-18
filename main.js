38b0123eecf59d32e4535c651f308d64ec77ce06
unknown <mledoshchuk@gmail.com>
2023-06-18 19:50:53 +0300

    Fixed main.js file

--- main.js
+++ main.js
@@ -43,33 +43,5 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
 
-        icon.addEventListener('mouseover', function handleClick(event) {
-            let className = icon.className;
-            let newContent = '';
-
-            if (className.includes("fa-cake-candles")) {
-                newContent = '<img src="assets/cat-cute.gif" alt="Hello">';
-            }
-            if (className.includes("fa-school")) {
-                newContent = '<img src="assets/school.gif" alt="School">';
-            }
-            if (className.includes("fa-graduation-cap")) {
-                newContent = '<img src="assets/university.gif" alt="University">';
-            }
-
-            if (className.includes("fa-cat")) {
-                newContent = '<img src="assets/cat.webp" alt="Cat">';
-            }
-
-            if (className.includes("fa-briefcase")) {
-                newContent = '<img src="assets/first-job.gif" alt="First Job">';
-            }
-
-            if (className.includes("fa-laptop")) {
-                newContent = '<img src="assets/monkey.gif" alt="Monkey">';
-            }
-
-        });
-
     });
 });
